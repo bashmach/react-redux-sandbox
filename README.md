5789473a10047fda983fd550001ab6d99d4b978c
Pavel Machekhin <pavel.machekhin@gmail.com>
2016-01-06 01:54:23 +0200

    Initial commit with Counter component and reducer

---
+++ README.md
@@ -0,0 +1,20 @@
+react-redux-sandbox
+=====================
+
+Sandbox for experiments with React and Redux based on [react-hot-boilerplate](https://github.com/gaearon/react-hot-boilerplate)
+
+### Usage
+
+```
+npm install
+npm start
+open http://localhost:3000
+```
+
+### Resources
+
+* [Demo video](http://vimeo.com/100010922)
+* [react-hot-loader on Github](https://github.com/gaearon/react-hot-loader)
+* [Integrating JSX live reload into your workflow](http://gaearon.github.io/react-hot-loader/getstarted/)
+* [Troubleshooting guide](https://github.com/gaearon/react-hot-loader/blob/master/docs/Troubleshooting.md)
+* Ping dan_abramov on Twitter or #reactjs IRC
---
+++ src/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { createStore } from 'redux';
+import ReactDOM from 'react-dom';
+
+const Counter = ({
+  value,
+  onIncrement,
+  onDecrement
+}) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+)
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+       return state + 1;
+
+    case 'DECREMENT':
+      return state - 1;
+
+    default:
+      return state;
+  }
+}
+
+const render = () => {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() =>
+        store.dispatch({
+          type: "INCREMENT"
+        })
+      }
+      onDecrement={() =>
+        store.dispatch({
+          type: "DECREMENT"
+        })
+      }
+    />,
+    document.getElementById('root'));
+}
+
+const store = createStore(counter);
+
+store.subscribe(render);
+
+render();
