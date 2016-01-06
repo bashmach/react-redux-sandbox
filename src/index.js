27f5db48d41a593a37026a41f4c2abfc1f423c0b
Pavel Machekhin <pavel.machekhin@gmail.com>
2016-01-06 02:35:51 +0200

    Redux DevTools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import React from 'react';
-import { createStore } from 'redux';
+import { createStore as originCreateStore, applyMiddleware, compose } from 'redux';
+
+import { createDevTools } from 'redux-devtools';
+import { persistState } from 'redux-devtools';
+
+import LogMonitor from 'redux-devtools-log-monitor';
+import DockMonitor from 'redux-devtools-dock-monitor';
+
 import ReactDOM from 'react-dom';
+
+// createDevTools takes a monitor and produces a DevTools component
+const DevTools = createDevTools(
+  // Monitors are individually adjustable with props.
+  // Consult their repositories to learn about those props.
+  // Here, we put LogMonitor inside a DockMonitor.
+  <DockMonitor toggleVisibilityKey='alt-w'
+               changePositionKey='ctrl-q'>
+    <LogMonitor theme='tomorrow' />
+  </DockMonitor>
+);
+
+let createStore = compose(
+  DevTools.instrument()
+)(originCreateStore);
 
 const Counter = ({
   value,
@@ -27,25 +49,28 @@ const counter = (state = 0, action) => {
   }
 }
 
+const store = createStore(counter);
+
 const render = () => {
   ReactDOM.render(
-    <Counter
-      value={store.getState()}
-      onIncrement={() =>
-        store.dispatch({
-          type: "INCREMENT"
-        })
-      }
-      onDecrement={() =>
-        store.dispatch({
-          type: "DECREMENT"
-        })
-      }
-    />,
+    <div>
+      <Counter
+        value={store.getState()}
+        onIncrement={() =>
+          store.dispatch({
+            type: "INCREMENT"
+          })
+        }
+        onDecrement={() =>
+          store.dispatch({
+            type: "DECREMENT"
+          })
+        }
+      />
+      <DevTools store={store}/>
+    </div>,
     document.getElementById('root'));
 }
-
-const store = createStore(counter);
 
 store.subscribe(render);
 
