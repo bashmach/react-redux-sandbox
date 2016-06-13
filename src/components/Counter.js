005d57db8230152aa97abfdfc2d3b44df9783787
Pavel Machekhin <pavel.machekhin@pipedrive.com>
2016-06-13 17:56:48 +0300

    Async actions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,28 +1,68 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, {Component, PropTypes} from 'react'
+import {connect} from 'react-redux';
 
-let Counter = ({id, value, dispatch}) => {
-  return (
-    <div>
-      <h1>{value}</h1>
-      <button onClick={e => {
-        e.preventDefault();
-        dispatch({
-          type: 'INCREMENT',
-          id
-        });
-      }}> INCR
-      </button>
-      <button onClick={e => {
-        e.preventDefault();
-        dispatch({
-          type: 'DECREMENT',
-          id
-        });
-      }}> DECR
-      </button>
-    </div>
-  );
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+
+  increment(e) {
+    e.preventDefault();
+
+    const {id, dispatch} = this.props;
+
+    dispatch({
+      type: 'INCREMENT_STARTED',
+      id
+    });
+
+    setTimeout(function () {
+      dispatch({
+        type: 'INCREMENT',
+        id
+      }).bind(this);
+    }, 2e3);
+  }
+
+  decrement(e) {
+    e.preventDefault();
+
+    const {id, dispatch} = this.props;
+
+    dispatch({
+      type: 'DECREMENT_STARTED',
+      id
+    });
+
+    setTimeout(function () {
+      dispatch({
+        type: 'DECREMENT',
+        id
+      }).bind(this);
+    }, 2e3);
+  }
+
+  render() {
+    const {value, isWaiting} = this.props;
+
+    return (
+      <div>
+        <h1>{value}{isWaiting && <span>...</span>}</h1>
+        <button disabled={isWaiting} onClick={this.increment}> INCR </button>
+        <button disabled={isWaiting} onClick={this.decrement}> DECR </button>
+      </div>
+    );
+  }
+}
+
+Counter.propTypes = {
+  id: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+  isWaiting: PropTypes.bool.isRequired,
+  dispatch: PropTypes.func.isRequired
 };
 
 export default connect()(Counter);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-const defaultState = [{id: 1, value: 0}, {id: 2, value: 0}];
+const defaultState = [{id: 1, value: 0, isWaiting: false}, {id: 2, value: 0, isWaiting: false}];
 
 const counter = (state = {}, action) => {
   if (state.id !== action.id) {
@@ -8,23 +8,33 @@ const counter = (state = {}, action) => {
   }
 
   switch (action.type) {
+    case 'INCREMENT_STARTED':
+    case 'DECREMENT_STARTED':
+      return {
+        ...state,
+        isWaiting: true
+      };
     case 'INCREMENT':
       return {
         ...state,
+        isWaiting: false,
         value: state.value + 1
       };
     case 'DECREMENT':
       return {
         ...state,
+        isWaiting: false,
         value: state.value - 1
       };
     default:
       return state;
   }
-}
+};
 
 const counters = (state = defaultState, action) => {
   switch (action.type) {
+    case 'INCREMENT_STARTED':
+    case 'DECREMENT_STARTED':
     case 'INCREMENT':
     case 'DECREMENT':
       return state.map(c => {
@@ -34,7 +44,7 @@ const counters = (state = defaultState, action) => {
     default:
       return state;
   }
-}
+};
 
 export default combineReducers({counters});
 
